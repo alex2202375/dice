f1bb142d4131405d3ff9d670cf2c285f0a97d415
sockyjam@126.com <sockyjam@SockyJamdeMacBook-Air.local>
2014-08-10 17:58:35 +0800

    add input edit box for username and password. Just for component,, not include the function.

--- Classes/LoginLayer.h
+++ Classes/LoginLayer.h
@@ -11,10 +11,12 @@
 
 #include <iostream>
 #include "CCLayer.h"
-
+#include "cocos-ext.h"
+#include "deprecated/CCDeprecated.h"
 using namespace cocos2d;
+using namespace cocos2d::extension;
 
-class LoginLayer: public Layer {
+class LoginLayer: public Layer{
 public:
     CREATE_FUNC(LoginLayer);
     virtual bool init() override;
